862e33ebcdd473db4314ebaaefef0ed7f17b9013
deathatred <maksym.linynskyi@gmail.com>
2025-09-12 18:09:41 +0300

    Small refactor

--- Assets/Scripts/Runtime/Firebase/FirebaseSaveLoadManager.cs
+++ Assets/Scripts/Runtime/Firebase/FirebaseSaveLoadManager.cs
@@ -1,6 +1,7 @@
 using Cysharp.Threading.Tasks;
 using Firebase;
 using Firebase.Database;
+using System;
 using System.Threading;
 using UnityEngine;
 
@@ -18,27 +19,51 @@ public class FirebaseSaveLoadManager
     public async UniTask SaveLevelDataToFirebaseAsync(int levelNumber, Stars starsCount, 
         CancellationTokenSource cts = default)
     {
-        var db = FirebaseBootstrap.Db;
-        var uid = FirebaseBootstrap.Uid;
+        try
+        {
+            var db = FirebaseBootstrap.Db;
+            var uid = FirebaseBootstrap.Uid;
 
-        string key = string.Format(LEVEL_STARS_COUNT_KEY, levelNumber);
-        await db.Child($"users/{uid}/{key}").SetValueAsync((int)starsCount).
-            AsUniTask().AttachExternalCancellation(cts.Token);
+            string key = string.Format(LEVEL_STARS_COUNT_KEY, levelNumber);
+            await db.Child($"users/{uid}/{key}").SetValueAsync((int)starsCount).
+                AsUniTask().AttachExternalCancellation(cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.LogWarning("Saving data from FB canceled");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Saving data erorred {ex}");
+        }
     }
     public async UniTask<int> LoadDataFromFromFirebaseAsync(int levelNumber, CancellationTokenSource cts = default)
     {
-        var db = FirebaseBootstrap.Db;
-        var uid = FirebaseBootstrap.Uid;
-
-        string key = string.Format(LEVEL_STARS_COUNT_KEY, levelNumber);
-        var snapshot = await db.Child($"users/{uid}/{key}").GetValueAsync().
-            AsUniTask().AttachExternalCancellation(cts.Token);      
-        if (snapshot.Exists && int.TryParse(snapshot.Value.ToString(), out int starsCount))
+        try
         {
-            return starsCount; 
-        }
+            var db = FirebaseBootstrap.Db;
+            var uid = FirebaseBootstrap.Uid;
 
-        return 0;
+            string key = string.Format(LEVEL_STARS_COUNT_KEY, levelNumber);
+            var snapshot = await db.Child($"users/{uid}/{key}").GetValueAsync().
+                AsUniTask().AttachExternalCancellation(cts.Token);
+            if (snapshot.Exists && int.TryParse(snapshot.Value.ToString(), out int starsCount))
+            {
+                return starsCount;
+            }
+
+            return 0;
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.LogWarning("Loading data from FB canceled");
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Loading data erorred {ex}");
+            return 0;
+        }
     }
     public async UniTask ClearAllDataFromFirebaseAsync(CancellationTokenSource cts = default)
     {
